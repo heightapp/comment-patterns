dea48ffec465792d93d28de14a6e245e4973c159
Sebastien Villar <seb@height.app>
2022-09-09 17:59:34 +0200

    Add support for extra extensions

--- languages/patterns/javascript.js
+++ languages/patterns/javascript.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "JavaScript",
-  nameMatchers: [".js", ".mjs"],
+  nameMatchers: [".js", ".mjs", ".cjs", ".jsx"],
   multiLineComment: require("./common/c-style.js").multiLine(),
   singleLineComment: require("./common/c-style.js").singleLine(),
 };
--- languages/patterns/typescript.js
+++ languages/patterns/typescript.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "TypeScript",
-  nameMatchers: [".ts"],
+  nameMatchers: [".ts", ".tsx"],
   multiLineComment: require("./common/c-style.js").multiLine(),
   singleLineComment: require("./common/c-style.js").singleLine(),
 };
